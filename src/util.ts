0fe46a0b0a76173b94e4b19e7b9284634b59751c
Ryunosuke Hashimoto <ryubb1108@gmail.com>
2022-03-16 12:48:40 +0900

    fix:pageIdを取得する正規表現の修正

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,13 @@
 function getPageId(url: string): string | null {
-  const pattern = /^.*-(\w+)$/;
-  const result = pattern.exec(url);
-  return result && result[1];
+  const pagePathPattern = /^https:\/\/www.notion.so\/.*\/(.+)$/;
+  const pagePathResult = pagePathPattern.exec(url);
+  if (!pagePathResult) return null;
+
+  const pagePath = pagePathResult && pagePathResult[1];
+  // includesで条件分岐せずにRegexを書きたい
+  const pageIdPattern = pagePath.includes("-") ? /^.*-(\w+)$$/ : /^(\w+)$/;
+  const pageIdResult = pageIdPattern.exec(pagePath);
+  return pageIdResult && pageIdResult[1];
 }
 
 exports.getPageId = getPageId;
